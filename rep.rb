ebd50f820980cf451cb52b1dd4a4e2d0942a810b
zaru <zarutofu@gmail.com>
2018-05-12 11:25:54 +0900

    rep version v0.1.1

--- rep.rb
+++ rep.rb
@@ -1,9 +1,9 @@
 class Rep < Formula
   desc "Initializes the GitHub repository. Labels and templates for Issue/PR."
   homepage "https://github.com/zaru/rep"
-  url "https://github.com/zaru/rep/releases/download/v0.1.0/rep_0.1.0_darwin_amd64.tar.gz"
-  version "0.1.0"
-  sha256 "f33e649f0bbb54d978377e304aeea9cb278d8e49893a6723ee271d82f86677e8"
+  url "https://github.com/zaru/rep/releases/download/v0.1.1/rep_0.1.1_darwin_amd64.tar.gz"
+  version "0.1.1"
+  sha256 "b54f211579f6aea1f2a28f6e55a3e0ce77cf0b86a7af6bfe3d0717551f7a5895"
 
   def install
     bin.install 'rep'
